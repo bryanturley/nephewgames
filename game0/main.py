de44488d84d5ca19cbbb49c0f5f6b3ca675a9368
Bryan Turley <bryanturley@gmail.com>
2022-08-16 14:17:04 -0500

    escape, also python is a garbage language

--- game0/main.py
+++ game0/main.py
@@ -2,7 +2,7 @@ import sys, pygame
 
 pygame.init()
 
-screen_size = screen_width, screen_height = 320, 240
+screen_size = screen_width, screen_height = 1728, 972
 
 
 
@@ -11,12 +11,13 @@ screen = pygame.display.set_mode(screen_size)
 
 
 
-
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-
+        if event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
+            if event.key == pygame.K_ESCAPE:
+                sys.exit()
 
     
     screen.fill((0, 0, 0))
