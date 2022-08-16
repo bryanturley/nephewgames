b1331440f357eec5a42c4065e621a8efe944734b
Bryan Turley <bryanturley@gmail.com>
2022-08-16 16:58:25 -0500

    BOOM BOOM BOOM, python is garbage

--- game0/main.py
+++ game0/main.py
@@ -1,4 +1,4 @@
-import math, sys, pygame
+import math, sys, pygame, random
 from dataclasses import dataclass
        
 pygame.init()
@@ -35,12 +35,14 @@ class Bullet:
     dx: float = 0
     dy: float = 0
     l: int = 0
-
+    sz: int = 4
 
 bullets = []
+explodes = []
 for i in range(MAX_BULLETS):
     bullets.append(Bullet())
-
+for i in range(MAX_BULLETS):
+    explodes.append(Bullet())
 
 
 def ship_motion():
@@ -68,6 +70,57 @@ def ship_motion():
     dx *= 0.9999
     dy *= 0.9999
 
+def explode_motion():
+    cnt = 0
+    for i in range(MAX_BULLETS):
+        if explodes[i].act == False:
+            continue
+        cnt += 1
+        explodes[i].x += explodes[i].dx
+        explodes[i].y += explodes[i].dy
+        explodes[i].l -= 8
+        if explodes[i].l < 2:
+            explodes[i].act = False
+        if explodes[i].x < 0:
+            explodes[i].x += screen_width
+        if explodes[i].x >= screen_width:
+            explodes[i].x -= screen_width
+        if explodes[i].y < 0:
+            explodes[i].y += screen_height
+        if explodes[i].y >= screen_height:
+            explodes[i].y -= screen_height
+        #print(cnt, " of ", MAX_BULLETS)
+
+def explode_start(x, y):
+    for j in range(16):
+        for i in range(MAX_BULLETS):
+            if explodes[i].act == False:
+                explodes[i].act = True
+                explodes[i].x = x + int(random.random() * 32 - 16)
+                explodes[i].y = y + int(random.random() * 32 - 16)
+                ang = random.random() * 360
+                mag = 8 + random.random() * 8
+                explodes[i].dx = math.cos(ang * 3.14 / 180) * mag
+                explodes[i].dy = math.sin(ang * 3.14 / 180) * mag
+                explodes[i].l = 255
+                explodes[i].sz = 4 + random.random() * 4
+                break
+
+        
+def explode_draw():
+    for i in range(MAX_BULLETS):
+        if explodes[i].act == False:
+            continue
+        start = (int(explodes[i].x), int(explodes[i].y))
+
+        l = explodes[i].l
+        if l < 72:
+            l = 72
+        color = (l/3, l, l/3)
+
+        pygame.draw.circle(screen, (255, l, l), start, int(explodes[i].sz), 2)
+
+        
 def bullet_motion():
     cnt = 0
     for i in range(MAX_BULLETS):
@@ -87,7 +140,7 @@ def bullet_motion():
             bullets[i].y += screen_height
         if bullets[i].y >= screen_height:
             bullets[i].y -= screen_height
-        print(cnt, " of ", MAX_BULLETS)
+        #print(cnt, " of ", MAX_BULLETS)
 
 def bullet_start(x, y, angle):
     for i in range(MAX_BULLETS):
@@ -99,6 +152,7 @@ def bullet_start(x, y, angle):
             bullets[i].dy = math.sin(angle * 3.14 / 180) * 16
             bullets[i].l = 255
             break
+        
 def bullet_draw():
     for i in range(MAX_BULLETS):
         if bullets[i].act == False:
@@ -112,6 +166,19 @@ def bullet_draw():
         color = (l/3, l, l/3)
 
         pygame.draw.line(screen, color, start, end, 3)
+
+def player_collision(tx, ty):
+    if tx <= x + 12 and tx >= x - 12 and ty <= y + 12 and ty >= y - 12:
+        return True
+    return False
+    
+def bullet_collision():
+    for i in range(MAX_BULLETS):
+        if bullets[i].act == False:
+            continue
+        if player_collision(bullets[i].x, bullets[i].y):
+            return True
+    return False
 
 
 #main loop
@@ -143,23 +210,31 @@ while 1:
         sx = x + math.cos(angle * 3.14 / 180) * 16
         sy = y + math.sin(angle * 3.14 / 180) * 16
         bullet_start(sx, sy, angle)
+        bullet_start(sx, sy, angle+1.75)
+        bullet_start(sx, sy, angle+0.75)
+        bullet_start(sx, sy, angle-0.75)
+        bullet_start(sx, sy, angle-1.75)
 
     ship_motion()
     bullet_motion()
-
+    explode_motion()
     
+    if bullet_collision():
+        explode_start(x, y)
+        dx = 0
+        dy = 0
+
     pygame.draw.circle(screen, (255, 255, 255), (int(x), int(y)), 16, 2)
     cx = int(x + math.cos(angle * 3.14 / 180) * 16)
     cy = int(y + math.sin(angle * 3.14 / 180) * 16)
     pygame.draw.circle(screen, (0, 255, 0), (cx, cy), 6, 2)
 
-
     bullet_draw()
-    
+    explode_draw()
     
     # flip double buffer
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(20)
     
     # blank screen
     screen.fill((0, 0, 0))
